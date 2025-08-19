0f1ebb08a6656a74319a9f17a04c867b72f59e6d
Elie Carlos <ecc226@nau.edu>
2025-08-19 12:48:35 -0700

    Chapter 1 Video 4

--- Start/Overview/GarbageCollect/Program.cs
+++ Start/Overview/GarbageCollect/Program.cs
@@ -5,7 +5,7 @@ void DoSomeBigOperation() {
     // create a large memory allocation that's only used in this function
     byte[] myArray = new byte[1000000];
 
-    Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
+    Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}"); // GC = Garbage Collector
     Console.ReadLine();
 }
 
@@ -16,7 +16,7 @@ Console.ReadLine();
 // Call the function that allocates a large memory chunk
 DoSomeBigOperation();
 // TODO: After the function completes, force a Garbage Collection 
-
+GC.Collect(); // probably never have to actually use this since it's managed automatically
 // Retrieve and print the updated total memory amount
 Console.WriteLine($"Allocated memory is: {GC.GetTotalMemory(false)}");
 Console.ReadLine();
