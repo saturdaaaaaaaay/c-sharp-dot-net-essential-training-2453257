bdc4671b9096478c6bea5194306c310ec8dfdef1
Elie Carlos <ecc226@nau.edu>
2025-08-19 13:34:58 -0700

    Chapter 2 Video 2

--- Start/Strings/Formatting/Program.cs
+++ Start/Strings/Formatting/Program.cs
@@ -5,7 +5,11 @@ float f1 = 123.4f;
 int i1 = 2000;
 
 // TODO: Spacing and Alignment: Indexes
-
+// float val: variable 0, negative = left aligned, 15 = width
+// int val: variable 1, positve = right aligned, 10 = width
+Console.WriteLine("{0, -15} {1, 10}", "Float Val", "Int Val");
+Console.WriteLine("{0, -15} {1, 10}", f1, i1);
 
 // TODO: Spacing and Alignment: Interpolation
-
+Console.WriteLine($"{"Float Val", -15} {"Int Val", 10}");
+Console.WriteLine($"{f1, -15} {i1, 10}");
